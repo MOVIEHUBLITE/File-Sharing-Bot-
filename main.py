de33705196441e9b68a8b6900cacece3f82f68ed
MoVIe HuB TeAm <92249328+MOVIEHUBLITE@users.noreply.github.com>
2022-05-30 01:40:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from variables import STAT_STICK, PICS, ADMIN, DELAY
 import asyncio
 import random
+
+
+Text = 
+
+
+@Client.on_message(filters.private & filters.command(["start"]))
+async def start(client,message):
+ await message.reply_text("Helo iam Youtube Video Search\nUse in inline mode")
+ 
