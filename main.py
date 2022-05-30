9f0a42a52dbc0cfa805f4c3e6eb00eff55b6364f
MoVIe HuB TeAm <92249328+MOVIEHUBLITE@users.noreply.github.com>
2022-05-30 20:22:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from pyrogram import Client
 
 TextBot = Client(
     "TextBot"
-    api_hash=
-    api_id=
-    bot_token=
+    api_hash="5820bc246505e0ff60af5391d649f9a6",
+    api_id="8406611",
+    bot_token="5302212624:AAE6L5B65dYqfyZdKACnkr8X7wJrgFd4SBk",
     plugins=dict(root="TextBot")
 )
 
