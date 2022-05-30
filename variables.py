bee109560e19b724490a264c9eccf365a4b89048
MoVIe HuB TeAm <92249328+MOVIEHUBLITE@users.noreply.github.com>
2022-05-30 20:45:47 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,3 +1,15 @@
 import re
 import os
 from os import environ
+
+
+
+id_pattern = re.compile(r'^.\d+$')
+
+STAT_STICK = os.environ.get("STAT_STICK", "CAACAgIAAxkBAAIER2KU31lwWVS9nDiKrEWNn6UVtbT3AAJoAAPb234AAf0R7Tldu9yTHgQ")
+
+PICS = os.environ.get("PICS", "https://telegra.ph/file/cb77c8e2ebf3473c9fbcf.jpg").split()
+
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMIN', '1467649219').split()]
+
+DELAY = int(os.environ.get("DELAY", "2"))
