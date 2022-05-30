7dc1603202c1aae3d8d64f3efae7010ad06925b2
MoVIe HuB TeAm <92249328+MOVIEHUBLITE@users.noreply.github.com>
2022-05-30 23:31:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 
 
 TextBot = Client(
-    "TextBot"
+    "TextBot",
     api_hash="5820bc246505e0ff60af5391d649f9a6",
     api_id="8406611",
     bot_token="5302212624:AAE6L5B65dYqfyZdKACnkr8X7wJrgFd4SBk",
