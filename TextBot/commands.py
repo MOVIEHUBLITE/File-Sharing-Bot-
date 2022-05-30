2d012f75212d9726d4af5ae9805a04276892eee8
MoVIe HuB TeAm <92249328+MOVIEHUBLITE@users.noreply.github.com>
2022-05-30 20:35:16 +0530

    Update commands.py

--- TextBot/commands.py
+++ TextBot/commands.py
@@ -1,6 +1,55 @@
 from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from variables import STAT_STICK, PICS, ADMIN, DELAY
+import asyncio
+import random
 
 
-@Client.on_message(filters.command("start"))
-async def start_cmd(bot, msg):
-    await msg.reply_text("Hello 🥺")
+WAIT_MSG = """"<b>Processing ...</b>"""
+
+
+
+@Client.on_message(filters.private & filters.command("start"))
+async def start_message(bot, message):
+    insert(int(message.chat.id))
+    await message.reply_chat_action("Typing")
+    await asyncio.sleep(DELAY)
+    m=await message.reply_sticker(STAT_STICK)
+    await asyncio.sleep(DELAY)
+    await m.delete()             
+    await message.reply_photo(
+        photo=random.choice(PICS),
+        caption=f"Hello {message.from_user.mention}👋🏻\nI'am A Multi use Bot with many usefull features.\neg:- Telegarph, Channel ID, User ID, Fun, Group Id etc...\nYou can see My commands by below button... \n\n◉ send channel last message with forwerd tag to get the channel id 💯",               
+        reply_markup=InlineKeyboardMarkup( [[
+            InlineKeyboardButton("❣️ 𝐒𝐔𝐏𝐏𝐎𝐑𝐓", url="https://t.me/BETA_BOTSUPPORT"),
+            InlineKeyboardButton("📢 𝐔𝐏𝐃𝐀𝐓𝐄𝐒", url="https://t.me/BETA_UPDATES")
+            ],[            
+            InlineKeyboardButton("ℹ️ 𝐇𝐄𝐋𝐏", callback_data="help"),
+            InlineKeyboardButton("😉 𝐅𝐔𝐍", callback_data="fun")
+            ],[
+            InlineKeyboardButton("👨‍💻 𝐃𝐄𝐕𝐒 👨‍💻 ", callback_data="devs"),
+            InlineKeyboardButton("🤖 𝐀𝐁𝐎𝐔𝐓", callback_data="about")
+            ]]
+            )
+        )
+         
+@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
+async def broadcast(bot, message):
+ if (message.reply_to_message):
+   ms = await message.reply_text("Geting All ids from database ...........")
+   ids = getid()
+   tot = len(ids)
+   await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
+   for id in ids:
+     try:
+     	await message.reply_to_message.copy(id)
+     except:
+     	pass
+
+
+@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["users"]))
+async def get_users(client: Client, message: Message):    
+    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
+    ids = getid()
+    tot = len(ids)
+    await msg.edit(f"Total uses = {tot}")
